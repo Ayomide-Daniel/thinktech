172d4945712f14d8cd2dd6a2408636b12baacc07
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-05-27 10:03:03 +0100

    nuxt patch

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - thinktech',
-    title: 'thinktech',
+    title: 'ThinkTech',
     htmlAttrs: {
       lang: 'en',
     },
@@ -16,14 +16,24 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;500;600;700&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/app.css', '@/assets/css/all.min.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/asset.js', '@/plugins/vuetify.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -37,15 +47,34 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          // featured_post: '#e1e2e4',
+          featured: '#f7f7f7',
+          postcard_grey: '#727377',
+          text: '#121212',
+          anchor: '#121212',
+          nav: '#eee',
+          aside: 'rgba(0, 0, 0, .6)',
+        },
         dark: {
+          anchor: '#fff',
+          text: '#fff',
+          featured: '#1b1b1e',
+          nav: '#242526',
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
@@ -54,6 +83,14 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
         },
+        options: {
+          themeCache: {
+            get: (key) => localStorage.getItem('dark_mode'),
+            set: (key, value) => localStorage.setItem('dark_mode', false),
+          },
+          customProperties: true,
+        },
+        treeShake: true,
       },
     },
   },
