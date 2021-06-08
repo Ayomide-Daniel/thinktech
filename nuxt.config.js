29450fdee514238631286b655f030d66a5e78b7c
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-06-08 16:12:42 +0100

    nuxt patch

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - thinktech',
+    titleTemplate: 'ThinkTech',
     title: 'ThinkTech',
     htmlAttrs: {
       lang: 'en',
